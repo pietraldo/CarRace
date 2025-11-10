14d445536f808885e468e9fac0c61c99f23b4f33
Grzesiek1212 <grzegorz.prasek3@gmail.com>
2025-11-10 19:41:59 +0100

    change inside of car

--- src/game/Scene.cpp
+++ src/game/Scene.cpp
@@ -155,7 +155,7 @@ void Scene::DrawModel(Shader& shader, Model& model)
 
 void Scene::CreateModels()
 {
-	const std::string carModelPath = "../assets/models/car/Untitled.gltf";
+	const std::string carModelPath = "../assets/models/car/car.gltf";
 	const std::string wheelModelPath = "../assets/models/wheel/wheel.gltf";
 
 	auto bodyModel = std::make_shared<Model>(carModelPath, glm::vec3(0.f, 0.0f, 0.f), 0.01f, glm::vec3(1.f));
@@ -184,7 +184,7 @@ void Scene::CreateLights()
 	AddLight(point_light1_ceneter_of_board);
 
 	/* Point light 2 - in the center of board */
-	Light* point_light2_ceneter_of_board = new LightPoint(glm::vec3(10.2f, 1.0f, 2.0f), glm::vec3(1.0f, 1.0f, 1.0f),
+	Light* point_light2_ceneter_of_board = new LightPoint(glm::vec3(10.2f, 2.0f, 2.0f), glm::vec3(1.0f, 1.0f, 1.0f),
 		1.0f, 0.09f, 0.032f, glm::vec3(0.0f, 0.0f, 0.0f),
 		glm::vec3(0.6f, 0.6f, 0.6f), glm::vec3(1.0f, 1.0f, 1.0f));
 	AddLight(point_light2_ceneter_of_board);
